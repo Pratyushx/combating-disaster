ed15b8b83708e17a42fc08888b4baf7eb1b7705e
Pratyush Sharma <sharmapratyush_bt2k16@dtu.ac.in>
2017-10-28 18:54:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+var accountSid = 'ACf2dbfeb17991968a14ed7990996ff5df'; 
+var authToken = '66ae5695d523db1386bc9c7e8ff14979'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+ 
+client.messages.create({ 
+    to: "+919136888968", 
+    from: "+12155158753", 
+    body: "Hello Dude :)", 
+}//, function(err, message) { 
+   // console.log(message.sid); 
+).then((message)=> console.log(message.sid));
